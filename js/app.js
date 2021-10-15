721381fe6015ccd1ae94180184d67041cb1d2006
Enrique Elizondo <quiqueev@gmail.com>
2021-10-15 11:28:49 -0500

    Controls & Axis

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.122.0/examples/jsm/controls/OrbitControls.js";
+
 // Create scene
 const scene = new THREE.Scene();
 
@@ -13,15 +15,27 @@ const camera = new THREE.PerspectiveCamera(
   1,
   500
 );
-camera.position.set(0, 0, 100);
+camera.position.set(0, 10, 100);
 camera.lookAt(0, 0, 0);
 
+// Axes helper
+const axesHelper = new THREE.AxesHelper(50);
+scene.add(axesHelper);
+// X -> RED
+// Y -> GREEN
+// Z -> BLUE
+
+var controls = new OrbitControls(camera, renderer.domElement);
+controls.minDistance = 2;
+controls.maxDistance = 500;
+controls.update();
+
 // Lineas
-const lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
+const lineMaterial = new THREE.LineBasicMaterial({ color: 0x8533ff });
 const points = [];
-points.push(new THREE.Vector3(-90, -25, 0));
+points.push(new THREE.Vector3(-60, 0, 60));
 points.push(new THREE.Vector3(0, 0, 0));
-points.push(new THREE.Vector3(90, -25, 0));
+points.push(new THREE.Vector3(60, 0, 60));
 const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
 const line = new THREE.Line(lineGeometry, lineMaterial);
 scene.add(line);
@@ -53,15 +67,16 @@ cube3.position.x = -60;
 scene.add(cube3);
 
 // Calle
-const streetMaterial = new THREE.MeshPhongMaterial({
-  color: 0x666666,
+const streetMaterial = new THREE.MeshBasicMaterial({
+  // Phong
+  color: 0x006666,
   side: THREE.DoubleSide
 });
 
 var streetPoints = [
-  new THREE.Vector3(-90, -25, -40),
-  new THREE.Vector3(0, 0, -40),
-  new THREE.Vector3(90, -25, -40)
+  new THREE.Vector3(-60, 0, 30),
+  new THREE.Vector3(0, 0, 0),
+  new THREE.Vector3(60, 0, 30)
 ];
 const streetGeo = new THREE.ShapeBufferGeometry(new THREE.Shape(streetPoints));
 const street = new THREE.Mesh(streetGeo, streetMaterial);
@@ -76,21 +91,17 @@ const dashedLineMaterial = new THREE.LineDashedMaterial({
   gapSize: 10
 });
 
-const dashedPoints = [
-  new THREE.Vector3(0, -25, -1),
-  new THREE.Vector3(0, 0, -1)
-];
+const dashedPoints = [new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 25)];
 const dashedLineGeometry = new THREE.BufferGeometry().setFromPoints(
   dashedPoints
 );
 const dashedLine1 = new THREE.Line(dashedLineGeometry, dashedLineMaterial);
 
 dashedLine1.computeLineDistances();
-dashedLine1.rotation.x += 5.5;
 scene.add(dashedLine1);
 
 // Luz faro
-const light1 = new THREE.PointLight(0xfff9d8, 0.7);
+const light1 = new THREE.PointLight(0x1f51ff, 0.7);
 light1.position.set(10, 10, 10);
 scene.add(light1);
 
@@ -98,9 +109,7 @@ const light2 = new THREE.PointLight(0xfff9d8, 0.7);
 light2.position.set(-10, 10, 10);
 scene.add(light2);
 
-console.log(window.innerWidth);
-
-let x = 0;
+// DRIVING WHEEL
 
 // Animate
 const animate = function() {
@@ -115,12 +124,11 @@ const animate = function() {
 };
 
 const animateLines = line => {
-  if (line.position.y <= -20) {
+  if (line.position.z >= 20) {
     line.position.y = 0;
-    line.position.z = 1;
+    line.position.z = 0;
   } else {
-    line.position.y -= 0.05;
-    line.position.z -= 0.01;
+    line.position.z += 0.07;
   }
 };
 
